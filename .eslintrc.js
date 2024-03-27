5953f93efb52c9f9a8dd23a971d954f0146f8766
django <six.django@gmail.com>
2024-03-27 15:09:18 +0800

    fix: form build error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,4 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ['plugin:react/recommended', 'standard-with-typescript', 'prettier', 'plugin:storybook/recommended'],
-  overrides: [],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: ['react'],
-  rules: {
-    'react/jsx-key': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': 'warn',
-    'no-console': 'warn',
-  },
+  root: true,
+  extends: ['@yc-tech/eslint-config'].map(require.resolve)
 }
--- src/lib/hooks/useForm.ts
+++ src/lib/hooks/useForm.ts
@@ -1,16 +1,19 @@
 import { FieldValues, UseFormProps, UseFormReturn, useForm as useRForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { z } from '@yc-tech/shared'
+import { z, ZodTypeDef } from '@yc-tech/shared'
 export function useForm<
   TFieldValues extends FieldValues = FieldValues,
   TContext = any,
   TTransformedValues extends FieldValues = TFieldValues,
+  Output = any,
+  Def extends ZodTypeDef = ZodTypeDef,
+  Input = Output
 >(
-  schema: z.Schema<any, any>,
-  props?: UseFormProps<TFieldValues, TContext>,
+  schema: z.Schema<Output, Def, Input>,
+  props?: UseFormProps<TFieldValues, TContext>
 ): UseFormReturn<TFieldValues, TContext, TTransformedValues> {
-  return useRForm<z.infer<typeof schema>>({
+  return useRForm<TFieldValues, TContext, TTransformedValues>({
     ...props,
-    resolver: zodResolver(schema),
-  }) as UseFormReturn<TFieldValues, TContext, TTransformedValues>
+    resolver: zodResolver(schema)
+  })
 }

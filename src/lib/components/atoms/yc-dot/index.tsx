81cf573b7e2c4ddf8956d91954806c75ec298042
django <six.django@gmail.com>
2024-04-09 16:14:44 +0800

    feat: 新增 yc dot 参数 show zero

--- src/lib/components/atoms/yc-dot/index.tsx
+++ src/lib/components/atoms/yc-dot/index.tsx
@@ -7,22 +7,34 @@ type YcDotProps = {
   y?: number
   value?: string | number
   dot?: boolean
+  showZero?: boolean
   show?: boolean
   wrapperClassName?: string
 } & React.HTMLAttributes<HTMLDivElement>
-export default function YcDot({ value, children, dot, y, x, show, className, wrapperClassName }: YcDotProps) {
+export function YcDot({
+  value,
+  showZero,
+  children,
+  dot,
+  y,
+  x,
+  show,
+  className,
+  wrapperClassName
+}: YcDotProps) {
   if (show === false) return children
   return (
     <div className={clsxm('relative')}>
       {children}
-      <div
-        className={clsxm('rounded-full absolute bg-white p-[2px]', wrapperClassName)}
-        style={{ top: y || 0, right: x || 0 }}
-      >
-        <div className={clsxm('rounded-full  bg-danger p-1 leading-none text-white', className)}>
-          {dot ? undefined : value}
+      {(value || showZero || dot) && (
+        <div
+          className={clsxm('rounded-full absolute bg-white p-[2px]', wrapperClassName)}
+          style={{ top: y || 0, right: x || 0 }}>
+          <div className={clsxm('rounded-full  bg-danger p-1 leading-none text-white', className)}>
+            {dot ? undefined : value}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   )
 }

f359a2eebfbc911819b31846ac29a6abc053aad5
django <six.django@gmail.com>
2024-04-26 11:21:41 +0800

    fix: pref dot

--- src/lib/components/atoms/yc-dot/index.tsx
+++ src/lib/components/atoms/yc-dot/index.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { clsxm } from '@yc-tech/shared'
+import { motion } from 'framer-motion'
 
 type YcDotProps = {
   x?: number
@@ -27,13 +28,23 @@ export function YcDot({
     <div className={clsxm('relative')}>
       {children}
       {(value || showZero || dot) && (
-        <div
+        <motion.div
+          initial={{
+            opacity: 0
+          }}
+          animate={{
+            opacity: 1
+          }}
           className={clsxm('rounded-full absolute bg-white p-[2px]', wrapperClassName)}
           style={{ top: y || 0, right: x || 0 }}>
-          <div className={clsxm('rounded-full  bg-danger p-1 leading-none text-white', className)}>
+          <div
+            className={clsxm(
+              'rounded-full  bg-danger p-1 leading-none text-white flex items-center justify-center w-full h-full',
+              className
+            )}>
             {dot ? undefined : value}
           </div>
-        </div>
+        </motion.div>
       )}
     </div>
   )

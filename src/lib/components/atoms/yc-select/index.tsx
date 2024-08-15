ce613305d11755a161e47b1773c78bd8e41efc79
django <six.django@gmail.com>
2024-08-15 15:37:32 +0800

    fix: change select

--- src/lib/components/atoms/yc-select/index.tsx
+++ src/lib/components/atoms/yc-select/index.tsx
@@ -1,60 +1,11 @@
 'use client'
 
-import RcSelect, { BaseSelectRef } from 'rc-select'
-import { BaseOptionType, DefaultOptionType, SelectProps } from 'rc-select/lib/Select'
-import './index.less'
-import { ArchiveIcon, ChevronDownIcon } from '@radix-ui/react-icons'
-import { YcIcon } from '../yc-icon'
-import { forwardRef } from 'react'
-import { YcSpinner } from '../yc-spinner'
+import { Select } from '@douyinfe/semi-ui'
+import { SelectProps, OptionProps } from '@douyinfe/semi-ui/lib/es/select'
+export type YcSelectOptionProps = OptionProps
 
-type YcSelectState = {
-  loading?: boolean
-  open?: boolean
-  focused?: boolean
-  showSearch?: boolean
-  searchValue: string
+export function YcSelect(props: SelectProps) {
+  return <Select {...props} />
 }
 
-export type YcSelectOptionType = BaseOptionType | DefaultOptionType
-
-type YcSelectProps<
-  ValueType = any,
-  OptionType extends YcSelectOptionType = DefaultOptionType
-> = SelectProps<ValueType, OptionType> & {
-  children?: React.ReactNode
-} & React.RefAttributes<BaseSelectRef>
-
-export const YcSelect = forwardRef<BaseSelectRef, YcSelectProps>(function ({ ...rest }, ref) {
-  const SuffixIcon = ({ loading }: YcSelectState) => {
-    if (loading) {
-      return <YcSpinner className="w-5 h-5" />
-    }
-    return <ChevronDownIcon className="w-5 h-5" />
-  }
-  const CloseIcon = () => <YcIcon icon="mingcute:close-line" className="w-3.5 h-3.5 " />
-  const EmptyNode = () => (
-    <div className="p-4 w-full h-full flex flex-col items-center justify-center">
-      <ArchiveIcon className="w-5 h-5 text-neutral-400 mx-auto" />
-      <div className="text-neutral-400 text-xs mt-2">No data available</div>
-    </div>
-  )
-  return (
-    <RcSelect
-      // allowClear={{ clearIcon: <CloseIcon /> }}
-      ref={ref}
-      placeholder="Select an option"
-      style={{ width: '100%' }}
-      animation="slide-up"
-      removeIcon={<CloseIcon />}
-      // showSearch
-      dropdownMatchSelectWidth={false}
-      dropdownStyle={{
-        width: 'auto'
-      }}
-      notFoundContent={<EmptyNode />}
-      menuItemSelectedIcon={false}
-      suffixIcon={SuffixIcon}
-      {...rest}></RcSelect>
-  )
-})
+YcSelect.displayName = 'YcSelect'

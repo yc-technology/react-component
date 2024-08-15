20254eb73d055f820aaa9bb533eea4adbc1ade3b
django <six.django@gmail.com>
2024-08-15 20:44:47 +0800

    feat: add local

---
+++ src/lib/components/atoms/locales/index.ts
@@ -0,0 +1,47 @@
+import zh_CN from '@douyinfe/semi-ui/lib/es/locale/source/zh_CN'
+import en_GB from '@douyinfe/semi-ui/lib/es/locale/source/en_GB'
+import en_US from '@douyinfe/semi-ui/lib/es/locale/source/en_US'
+import ko_KR from '@douyinfe/semi-ui/lib/es/locale/source/ko_KR'
+import ja_JP from '@douyinfe/semi-ui/lib/es/locale/source/ja_JP'
+import vi_VN from '@douyinfe/semi-ui/lib/es/locale/source/vi_VN'
+import ru_RU from '@douyinfe/semi-ui/lib/es/locale/source/ru_RU'
+import id_ID from '@douyinfe/semi-ui/lib/es/locale/source/id_ID'
+import ms_MY from '@douyinfe/semi-ui/lib/es/locale/source/ms_MY'
+import th_TH from '@douyinfe/semi-ui/lib/es/locale/source/th_TH'
+import tr_TR from '@douyinfe/semi-ui/lib/es/locale/source/tr_TR'
+import pt_BR from '@douyinfe/semi-ui/lib/es/locale/source/pt_BR'
+import zh_TW from '@douyinfe/semi-ui/lib/es/locale/source/zh_TW'
+import sv_SE from '@douyinfe/semi-ui/lib/es/locale/source/sv_SE'
+import pl_PL from '@douyinfe/semi-ui/lib/es/locale/source/pl_PL'
+import nl_NL from '@douyinfe/semi-ui/lib/es/locale/source/nl_NL'
+import ar from '@douyinfe/semi-ui/lib/es/locale/source/ar'
+import es from '@douyinfe/semi-ui/lib/es/locale/source/es'
+import it from '@douyinfe/semi-ui/lib/es/locale/source/it'
+import de from '@douyinfe/semi-ui/lib/es/locale/source/de'
+import fr from '@douyinfe/semi-ui/lib/es/locale/source/fr'
+import ro from '@douyinfe/semi-ui/lib/es/locale/source/ro'
+
+export default {
+  zh_CN: zh_CN,
+  en_GB: en_GB,
+  en_US: en_US,
+  ko_KR: ko_KR,
+  ja_JP: ja_JP,
+  vi_VN: vi_VN,
+  ru_RU: ru_RU,
+  id_ID: id_ID,
+  ms_MY: ms_MY,
+  th_TH: th_TH,
+  tr_TR: tr_TR,
+  pt_BR: pt_BR,
+  zh_TW: zh_TW,
+  sv_SE: sv_SE,
+  pl_PL: pl_PL,
+  nl_NL: nl_NL,
+  ar: ar,
+  es: es,
+  it: it,
+  de: de,
+  fr: fr,
+  ro: ro
+}

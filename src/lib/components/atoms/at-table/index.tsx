ae6b86111010c989c2bf995a4d8021b3538b8b1d
django <six.django@gmail.com>
2024-08-15 16:52:02 +0800

    feat: add at table

--- src/lib/components/atoms/at-table/index.tsx
+++ src/lib/components/atoms/at-table/index.tsx
@@ -1,19 +1,20 @@
 import { Table } from '@douyinfe/semi-ui'
-import { Data } from '@douyinfe/semi-ui/lib/es/table'
-import { TableProps, ColumnProps } from '@douyinfe/semi-ui/lib/es/table'
-import NormalTable from '@douyinfe/semi-ui/lib/es/table/Table'
-import { forwardRef, ReactElement, Ref, useRef } from 'react'
+import { ColumnProps, Data, TableProps } from '@douyinfe/semi-ui/lib/es/table'
+import { forwardRef, ReactElement, Ref, useImperativeHandle } from 'react'
 
 export type AtColumnProps<T extends Record<string, any> = Data> = ColumnProps<T>
-export type AtTableRef<T extends Record<string, any> = Data> = NormalTable<T>
+export type AtTableRef<T extends Record<string, any> = Data> = {
+  columns?: AtColumnProps<T>[]
+}
 
 function AtTableComp<T extends Record<string, any> = Data>(
   props: TableProps<T>,
   ref: Ref<AtTableRef<T>>
 ) {
-  return <Table<T> tableRef={ref} {...props} />
+  useImperativeHandle(ref, () => ({ columns: props.columns }), [ref])
+  return <Table<T> {...props} />
 }
 
 export const AtTable = forwardRef(AtTableComp) as <T extends Record<string, any> = Data>(
-  p: TableProps<T> & { ref?: Ref<Table<T>> }
+  p: TableProps<T> & { ref?: Ref<AtTableRef<T>> }
 ) => ReactElement

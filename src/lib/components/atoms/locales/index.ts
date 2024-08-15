4a4ce66630bf75bcfae39ea1a5cc6ed9f2a60324
django <six.django@gmail.com>
2024-08-15 20:50:02 +0800

    feat: add local

--- src/lib/components/atoms/locales/index.ts
+++ src/lib/components/atoms/locales/index.ts
@@ -1,47 +1,45 @@
-import zh_CN from '@douyinfe/semi-ui/lib/es/locale/source/zh_CN'
-import en_GB from '@douyinfe/semi-ui/lib/es/locale/source/en_GB'
-import en_US from '@douyinfe/semi-ui/lib/es/locale/source/en_US'
-import ko_KR from '@douyinfe/semi-ui/lib/es/locale/source/ko_KR'
-import ja_JP from '@douyinfe/semi-ui/lib/es/locale/source/ja_JP'
-import vi_VN from '@douyinfe/semi-ui/lib/es/locale/source/vi_VN'
-import ru_RU from '@douyinfe/semi-ui/lib/es/locale/source/ru_RU'
-import id_ID from '@douyinfe/semi-ui/lib/es/locale/source/id_ID'
-import ms_MY from '@douyinfe/semi-ui/lib/es/locale/source/ms_MY'
-import th_TH from '@douyinfe/semi-ui/lib/es/locale/source/th_TH'
-import tr_TR from '@douyinfe/semi-ui/lib/es/locale/source/tr_TR'
-import pt_BR from '@douyinfe/semi-ui/lib/es/locale/source/pt_BR'
-import zh_TW from '@douyinfe/semi-ui/lib/es/locale/source/zh_TW'
-import sv_SE from '@douyinfe/semi-ui/lib/es/locale/source/sv_SE'
-import pl_PL from '@douyinfe/semi-ui/lib/es/locale/source/pl_PL'
-import nl_NL from '@douyinfe/semi-ui/lib/es/locale/source/nl_NL'
-import ar from '@douyinfe/semi-ui/lib/es/locale/source/ar'
-import es from '@douyinfe/semi-ui/lib/es/locale/source/es'
-import it from '@douyinfe/semi-ui/lib/es/locale/source/it'
-import de from '@douyinfe/semi-ui/lib/es/locale/source/de'
-import fr from '@douyinfe/semi-ui/lib/es/locale/source/fr'
-import ro from '@douyinfe/semi-ui/lib/es/locale/source/ro'
+import _zh_CN from '@douyinfe/semi-ui/lib/es/locale/source/zh_CN'
+import _en_GB from '@douyinfe/semi-ui/lib/es/locale/source/en_GB'
+import _en_US from '@douyinfe/semi-ui/lib/es/locale/source/en_US'
+import _ko_KR from '@douyinfe/semi-ui/lib/es/locale/source/ko_KR'
+import _ja_JP from '@douyinfe/semi-ui/lib/es/locale/source/ja_JP'
+import _vi_VN from '@douyinfe/semi-ui/lib/es/locale/source/vi_VN'
+import _ru_RU from '@douyinfe/semi-ui/lib/es/locale/source/ru_RU'
+import _id_ID from '@douyinfe/semi-ui/lib/es/locale/source/id_ID'
+import _ms_MY from '@douyinfe/semi-ui/lib/es/locale/source/ms_MY'
+import _th_TH from '@douyinfe/semi-ui/lib/es/locale/source/th_TH'
+import _tr_TR from '@douyinfe/semi-ui/lib/es/locale/source/tr_TR'
+import _pt_BR from '@douyinfe/semi-ui/lib/es/locale/source/pt_BR'
+import _zh_TW from '@douyinfe/semi-ui/lib/es/locale/source/zh_TW'
+import _sv_SE from '@douyinfe/semi-ui/lib/es/locale/source/sv_SE'
+import _pl_PL from '@douyinfe/semi-ui/lib/es/locale/source/pl_PL'
+import _nl_NL from '@douyinfe/semi-ui/lib/es/locale/source/nl_NL'
+import _ar from '@douyinfe/semi-ui/lib/es/locale/source/ar'
+import _es from '@douyinfe/semi-ui/lib/es/locale/source/es'
+import _it from '@douyinfe/semi-ui/lib/es/locale/source/it'
+import _de from '@douyinfe/semi-ui/lib/es/locale/source/de'
+import _fr from '@douyinfe/semi-ui/lib/es/locale/source/fr'
+import _ro from '@douyinfe/semi-ui/lib/es/locale/source/ro'
 
-export default {
-  zh_CN: zh_CN,
-  en_GB: en_GB,
-  en_US: en_US,
-  ko_KR: ko_KR,
-  ja_JP: ja_JP,
-  vi_VN: vi_VN,
-  ru_RU: ru_RU,
-  id_ID: id_ID,
-  ms_MY: ms_MY,
-  th_TH: th_TH,
-  tr_TR: tr_TR,
-  pt_BR: pt_BR,
-  zh_TW: zh_TW,
-  sv_SE: sv_SE,
-  pl_PL: pl_PL,
-  nl_NL: nl_NL,
-  ar: ar,
-  es: es,
-  it: it,
-  de: de,
-  fr: fr,
-  ro: ro
-}
+export const zh_CN = _zh_CN
+export const en_GB = _en_GB
+export const en_US = _en_US
+export const ko_KR = _ko_KR
+export const ja_JP = _ja_JP
+export const vi_VN = _vi_VN
+export const ru_RU = _ru_RU
+export const id_ID = _id_ID
+export const ms_MY = _ms_MY
+export const th_TH = _th_TH
+export const tr_TR = _tr_TR
+export const pt_BR = _pt_BR
+export const zh_TW = _zh_TW
+export const sv_SE = _sv_SE
+export const pl_PL = _pl_PL
+export const nl_NL = _nl_NL
+export const ar = _ar
+export const es = _es
+export const it = _it
+export const de = _de
+export const fr = _fr
+export const ro = _ro

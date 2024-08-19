d34ca7c974194cc7f26e63afec3c05e1e9b67c34
django <six.django@gmail.com>
2024-08-19 15:25:32 +0800

    fix: delete some type

--- src/lib/components/atoms/at-form/index.tsx
+++ src/lib/components/atoms/at-form/index.tsx
@@ -5,34 +5,34 @@ export type AtFormApi<T extends object = any> = FormApi<T>
 
 export const AtForm = Form
 
-AtForm.Input = Form.Input
-AtForm.InputNumber = Form.InputNumber
-AtForm.InputGroup = Form.InputGroup
+// AtForm.Input = Form.Input
+// AtForm.InputNumber = Form.InputNumber
+// AtForm.InputGroup = Form.InputGroup
 
-AtForm.Label = Form.Label
+// AtForm.Label = Form.Label
 
-AtForm.Radio = Form.Radio
-AtForm.RadioGroup = Form.RadioGroup
-AtForm.Rating = Form.Rating
+// AtForm.Radio = Form.Radio
+// AtForm.RadioGroup = Form.RadioGroup
+// AtForm.Rating = Form.Rating
 
-AtForm.Section = Form.Section
-AtForm.Select = Form.Select
-AtForm.Switch = Form.Switch
-AtForm.Slider = Form.Slider
-AtForm.Slot = Form.Slot
+// AtForm.Section = Form.Section
+// AtForm.Select = Form.Select
+// AtForm.Switch = Form.Switch
+// AtForm.Slider = Form.Slider
+// AtForm.Slot = Form.Slot
 
-AtForm.Checkbox = Form.Checkbox
-AtForm.Cascader = Form.Cascader
-AtForm.CheckboxGroup = Form.CheckboxGroup
+// AtForm.Checkbox = Form.Checkbox
+// AtForm.Cascader = Form.Cascader
+// AtForm.CheckboxGroup = Form.CheckboxGroup
 
-AtForm.TextArea = Form.TextArea
-AtForm.TimePicker = Form.TimePicker
-AtForm.TagInput = Form.TagInput
-AtForm.TreeSelect = Form.TreeSelect
+// AtForm.TextArea = Form.TextArea
+// AtForm.TimePicker = Form.TimePicker
+// AtForm.TagInput = Form.TagInput
+// AtForm.TreeSelect = Form.TreeSelect
 
-AtForm.DatePicker = Form.DatePicker
+// AtForm.DatePicker = Form.DatePicker
 
-AtForm.AutoComplete = Form.AutoComplete
+// AtForm.AutoComplete = Form.AutoComplete
 
-AtForm.ErrorMessage = Form.ErrorMessage
-AtForm.Upload = Form.Upload
+// AtForm.ErrorMessage = Form.ErrorMessage
+// AtForm.Upload = Form.Upload

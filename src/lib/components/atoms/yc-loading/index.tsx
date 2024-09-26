5882c973539e0128b8894114cc155952ba9bbb98
django <six.django@gmail.com>
2024-09-26 14:14:36 +0800

    fix: loading css

--- src/lib/components/atoms/yc-loading/index.tsx
+++ src/lib/components/atoms/yc-loading/index.tsx
@@ -6,16 +6,21 @@ import { YcSpinner } from '../yc-spinner'
 interface YcLoadingProps {
   children: React.ReactNode
   className?: string
+  loadingClassName?: string
   loading?: boolean
   lazy?: boolean
 }
 export function YcLoading(props: YcLoadingProps) {
-  const { children, lazy, className, loading } = props
+  const { children, lazy, className, loadingClassName, loading } = props
   return (
     <div className={clsxm(className, 'relative')}>
       {((lazy && !loading) || !lazy) && children}
       {loading && (
-        <div className=" absolute top-0 left-0 w-full h-full flex justify-center items-center">
+        <div
+          className={clsxm(
+            'absolute top-0 left-0 bg-white/40 w-full h-full flex justify-center items-center',
+            loadingClassName
+          )}>
           <YcSpinner className=" m-auto h-6 w-6"></YcSpinner>
         </div>
       )}
